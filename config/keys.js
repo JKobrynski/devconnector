55bf3c1a02e923fb2fedcba9e0eff50bd9b4d1d9
JKobrynski <kobrynski@onet.pl>
2018-12-25 20:00:40 +0100

    Updated login route (added JWT)

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://kobra:kobra123@ds159782.mlab.com:59782/devconnector_jk"
+  mongoURI: "mongodb://kobra:kobra123@ds159782.mlab.com:59782/devconnector_jk",
+  secretOrKey: "secret"
 };
